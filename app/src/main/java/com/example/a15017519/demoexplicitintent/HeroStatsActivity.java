ab6cbe21589aedd2a209fe7f6285e60dd97a50b6
15017519 <15017519@myrp.edu.sg>
2017-05-05 10:48:54 +0800

    commited

--- app/src/main/java/com/example/a15017519/demoexplicitintent/HeroStatsActivity.java
+++ app/src/main/java/com/example/a15017519/demoexplicitintent/HeroStatsActivity.java
@@ -11,7 +11,6 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class HeroStatsActivity extends AppCompatActivity {
-
     TextView tvName, tvStrength, tvTechnicalProwess;
     Button btnLike, btnDislike;
 
@@ -19,7 +18,6 @@ public class HeroStatsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hero_stats);
-
         // Get the intent
         Intent i = getIntent();
         // Get the Hero object first activity put in Intent
@@ -28,12 +26,40 @@ public class HeroStatsActivity extends AppCompatActivity {
         tvName = (TextView) findViewById(R.id.textViewName);
         tvStrength = (TextView)findViewById(R.id.textViewStrength);
         tvTechnicalProwess = (TextView) 							findViewById(R.id.textViewTechnicalProwess);
+        btnLike = (Button) findViewById(R.id.buttonLike);
+        btnDislike = (Button) findViewById(R.id.buttonDislike);
 
-        // Use getters of Hero object to get the attributes
         tvName.setText(hero.getName());
         tvStrength.setText("Strength: " + hero.getStrength());
-        tvTechnicalProwess.setText("Technical: " +
-                hero.getTechnicalProwess());
-    }
+        tvTechnicalProwess.setText("Technical: " + 					hero.getTechnicalProwess());
 
+        // When button Like is clicked, set the results
+        //  accordingly and finish() to close this act.
+        btnLike.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                // Create intent & pass in String data
+                Intent i = new Intent();
+                i.putExtra("like", "like");
+                // Set result to RESULT_OK to indicate normal			// response and pass in the intent containing the 		// like
+                setResult(RESULT_OK, i);
+                finish();
+            }});
+
+        // When button Dislike is clicked, set the results
+        // accordingly and finish() to close this activity
+        btnDislike.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                //Create intent & pass in String data
+                Intent i = new Intent();
+                i.putExtra("like", "dislike");
+                // Set result to RESULT_OK to indicate normal
+                // response and pass in the intent containing the
+                // dislike
+                setResult(RESULT_OK, i);
+                finish();
+            }});
+    }
 }
+
